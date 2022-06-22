7580e2ab86afa796f875da095db8cdfaa8bd7c42
grantjensen <58962643+grantjensen@users.noreply.github.com>
2022-06-22 10:54:55 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import chess
 import chess.svg
-board = chess.Board()
-board.push_san("e4")
-boardsvg = chess.svg.board(board)
-with open('board.svg', 'w') as f:
-  f.write(boardsvg)
+if __name__ == "__main__":
+  board = chess.Board()
+  board.push_san("e4")
+  boardsvg = chess.svg.board(board)
+  with open('board.svg', 'w') as f:
+    f.write(boardsvg)
