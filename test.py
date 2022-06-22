3bb6291ed6c35c68980f369dd9bf8c80c0ea2a68
grantjensen <58962643+grantjensen@users.noreply.github.com>
2022-06-22 15:07:37 -0700

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import chess
-import chess.svg
-if __name__ == "__main__":
-  board = chess.Board()
-  board.push_san("e4")
-  boardsvg = chess.svg.board(board)
-  with open('board.svg', 'w') as f:
-    f.write(boardsvg)
